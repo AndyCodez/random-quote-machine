04967d807befd2d9327c5cfc4eb8b6d01f882ca5
AndyCodez <andystudy4@gmail.com>
2016-12-31 00:55:59 +0300

    enable tweeting from the app

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
 	$("#newQuote").click(function(){
 		$.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json){
 			var html = "";
+			var tweetHtml = "";
 
 					html += "\"" + json.quote + "\" <br><br>"; //show quote
 					html += "- " + json.author; //show the author of the quote
+					
+					tweetHtml += "<a class='button' target='_blank' href='https://twitter.com/intent/tweet?text=" + json.quote + " - " + json.author + " via @RealAndrewOwino'>Tweet</a>";
+
 					$(".quote").html(html);	//display entire html as under the class "quote"
+					$(".tweet").html(tweetHtml);
 				});
 	});
 });
